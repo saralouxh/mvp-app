f137ae3b87dcf55e8c02a6da87e3881f79a13b4e
Sara Hampson <shampson518@gmail.com>
2023-03-23 18:51:55 -0500

    Added models
    added validations

---
+++ app/models/rating.rb
@@ -0,0 +1,6 @@
+class Rating < ApplicationRecord
+    belongs_to :movie
+    belongs_to :user
+
+    validates: :rating, presence: true
+end
---
+++ spec/factories/ratings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :rating do
+    rating { 1 }
+  end
+end
