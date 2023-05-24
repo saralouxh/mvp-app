52087aff473b79622824d3a22965cbca9c8274a5
Sara Hampson <shampson518@gmail.com>
2023-05-24 11:42:12 -0500

    update commit for deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,3 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :bigint           not null, primary key
-#  email           :string
-#  first_name      :string
-#  last_name       :string
-#  password_digest :string
-#  token           :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email  (email)
-#  index_users_on_token  (token)
-#
-
 # The model that represents the User
 class User < ApplicationRecord
   has_secure_password validations: true
